cdb197685913eef7499c14f25ac7cdb5498f1614
Ilya <anzhinilya2@gmail.com>
2024-07-12 02:03:11 +0700

    token update

--- code/main.go
+++ code/main.go
@@ -29,7 +29,7 @@ func main() {
 		return
 	}
 
-	bot, err := tgbotapi.NewBotAPI(TEST_TOKEN)
+	bot, err := tgbotapi.NewBotAPI(TOKEN)
 	NotifyService = notify_service.NewNotifyService(bot)
 
 	if err != nil {
